e3b6a9a49dcc668338fff4f6159ffbfe105f6f30
taylor <taylor@baremetalcloud.com>
2019-11-24 02:32:51 -0500

    solution for captains room.

--- hackerRank/practice/theCaptinsRoom.py
+++ hackerRank/practice/theCaptinsRoom.py
@@ -1,12 +1,14 @@
-k = 5
-roomList = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
+# k = 5
+# roomList = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
 
-input()
-group = input().strip().split()
-rooms = set(group)
+nom = int(input())
+members = list(map(int, input().split()))
+rooms = set()  # Contains all the rooms.
+room_more_mem = set()  # Contains only the rooms with families.
 
-for r in rooms:
-    group.remove(r)
-
-non_captain = set(group)
-print(rooms.difference(non_captain).pop())
+for m in members:
+    if m not in rooms:
+        rooms.add(m)
+    else:
+        room_more_mem.add(m)
+print(rooms.difference(room_more_mem).pop())
