f5dcb7a8802e5c81e58fff1138cf8b8bbaa7e8d1
Taylor Gagne <tgagne@netsurion.com>
2019-12-18 16:38:46 -0500

    created directory interview directory for hackerrank and problem solving. solution for solveMeFirst.js

---
+++ aws/phoneInterviewPrep1.js
@@ -0,0 +1,11 @@
+const studentArr = [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]];
+
+function averageStudentScore(items) {
+    let map = new Map();
+
+    let studentMap =  items.map();
+
+    let mapAsc = new Map([...map.it])
+}
+
+averageStudentScore(studentArr);
