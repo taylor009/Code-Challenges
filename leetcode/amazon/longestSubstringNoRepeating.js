db504ebc6570e06bd9b54f855f17fceadcbda3df
Taylor Gagne <tgagne@netsurion.com>
2019-12-19 16:42:12 -0500

    solution for longestSubstringNoRepeating.js

---
+++ leetcode/amazon/longestSubstringNoRepeating.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+s = "bbbb";
+
+const lengthOfLongestSubstring = s => {
+    let map = {};
+    let start = 0;
+    let maxLen = 0;
+    let arr = s.split('');
+
+    for (let i=0; i < s.length; i++) {
+        let current = map[arr[i]];
+        if (current!=null && start <= current) {
+            start = current + 1
+        } else {
+            maxLen = Math.max(maxLen, i - start + 1)
+        }
+
+        map[arr[i]] = i
+    }
+
+    return maxLen
+};
+
+lengthOfLongestSubstring(s);
---
+++ leetcode/js/longestPalindromicString.js
@@ -0,0 +1,7 @@
+let s = 'babad';
+
+const longestPalindrome = s => {
+    console.log(s);
+};
+
+longestPalindrome(s);
