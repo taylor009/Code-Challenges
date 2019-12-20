805d74120719f72c8ec8b5755591b32c4cfed887
Taylor Gagne <tgagne@netsurion.com>
2019-12-20 16:29:29 -0500

    solution for commonPrefix.js

--- leetcode/js/commonPrefix.js
+++ leetcode/js/commonPrefix.js
@@ -14,10 +14,10 @@ const longestCommonPrefix = strs => {
     let isFound=false;
     let str=firstStr;
 
-    for(var j=firstStr.length;j>0;j--){
+    for(let j=firstStr.length; j>0; j--){
         str=firstStr.slice(0,j);
         isFound=strs.every((item)=>{
-            return item.indexOf(str)==0
+            return item.indexOf(str)===0
         });
         if(isFound){
             return str
