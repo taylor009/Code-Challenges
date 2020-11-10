376ef7951208c526181f79bc730087edff2ef0f3
Taylor Gagne <taylor@baremetalcloud.com>
2020-11-10 02:15:22 -0500

    divisibleSumPairs.js

---
+++ hackerRank/problemSolving/divisibleSumPairs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Complete the divisibleSumPairs function below.
+function divisibleSumPairs(n, k, ar) {
+    let count = 0;
+    if(ar.length === 0) return;
+    for(let i =0; i < n; i++){
+        for(let j = (i+1); j < n; j++){
+            if((ar[i] + ar[j]) % k === 0) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+
+
