c855dc67b249fc5e776f3e505778251dd45c053e
Taylor Gagne <taylor@baremetalcloud.com>
2020-11-10 02:49:00 -0500

    lengthOfLastWord.js

---
+++ leetcode/js/lengthOfLastWord.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const words = s.trim().split(" ");
+
+    return words.length > 0 ? words[words.length-1].length : 0;
+};
