5bb90ff5d72939f65c153010db3f170d1b07b3ee
Taylor <taylor@baremetalcloud.com>
2021-01-04 03:09:07 -0500

    Solved two number sum brute force solution from algo expert

---
+++ algoExpert/python/twoNumberSum/s1.py
@@ -0,0 +1,9 @@
+def twoNumberSum(array, targetSum):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
+    pass
