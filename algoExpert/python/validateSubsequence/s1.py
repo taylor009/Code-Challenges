1ef4b9ce90aa672b1fe317d913e09ab0d7c4bf37
Taylor <taylor@baremetalcloud.com>
2021-01-04 03:36:55 -0500

    Solved all variants of validating subsequences from algo expert

---
+++ algoExpert/python/validateSubsequence/s1.py
@@ -0,0 +1,11 @@
+# O(n) time | O(1) space
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence) - 1
+
+    pass
---
+++ algoExpert/python/validateSubsequence/s2.py
@@ -0,0 +1,11 @@
+# O(n) time | O(1) space
+def isValidSubsequence(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
+
+    pass
