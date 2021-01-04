e44fe6fbdca2c38ef2935a563882ca93f89900a3
Taylor <taylor@baremetalcloud.com>
2021-01-04 02:42:52 -0500

    Solved boats to save people from leetcode #881

---
+++ leetcode/python/boatsToSavePeople.py
@@ -0,0 +1,23 @@
+# Time complexity: O(N log(n))
+# Space complexity: O(N)
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+
+        left = 0
+        right = len(people)-1
+
+        boats_number = 0
+
+        while(left<=right):
+            if(left==right):
+                boats_number+=1
+                break
+            if(people[left]+people[right]<=limit):
+                left+=1
+
+            right-=1
+            boats_number+=1
+        return boats_number
