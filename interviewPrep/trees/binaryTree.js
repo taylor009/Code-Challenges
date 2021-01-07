e953b76cfe40f90df82589cefa92b00692424881
Taylor <taylor@baremetalcloud.com>
2021-01-07 18:58:14 -0500

    completed binary search tree intro.

---
+++ interviewPrep/trees/binaryTree.js
@@ -0,0 +1,24 @@
+/**
+ * Better than O(n)
+ * Ordered
+ * Flexible Size
+ *
+ * left side of the tree should be decreasing
+ *
+ * right side of the tree should be increasing
+ *
+ * Binary Search Tree
+ * @param value
+ * @constructor
+ *
+ * Lookup O(logN)
+ * Insert O(logN)
+ * Delete 0(logN)
+ */
+
+
+function BinaryTreeNode(value){
+	this.value;
+	this.left = null;
+	this.right = null;
+}
