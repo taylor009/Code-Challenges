e2dec98cbcb257eaeda48319034f881a00156b3c
Taylor <taylor@baremetalcloud.com>
2021-01-08 00:19:22 -0500

    completed linear search.

---
+++ interviewPrep/BFS&DFS/linearSearch.js
@@ -0,0 +1,14 @@
+let beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie'];
+
+beasts.indexOf('Godzilla');
+
+beasts.findIndex(function(item){
+	return item === 'Godzilla';
+});
+
+beasts.find(function(item){
+	return item === 'Godzilla';
+})
+
+beasts.includes('Godzilla')
+
