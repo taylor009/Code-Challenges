b766cb6bfc41c5e51b7f2e712aa12d27ab3e9ed3
Taylor <taylor@baremetalcloud.com>
2021-01-08 00:52:33 -0500

    solved fibonacci with dynamic programming.

---
+++ interviewPrep/dynamicProgramming/fibonacci.js
@@ -0,0 +1,25 @@
+let calculations = 0;
+function fibonacci(n){
+	calculations++
+	if(n < 2){
+		return n;
+	}
+	return fibonacci(n-1) + fibonacci(n-2);
+}
+
+function fibonacciMaster(){
+	let cache = {};
+	return function fib(n){
+		if(n in cache){
+			return cache[n];
+		} else {
+			if(n < 2){
+				return n;
+			}
+			else {
+				cache[n] = fib(n-1) + fib(n-2);
+				return cache[n];
+			}
+		}
+	}
+}
