1a05c5015fd2da07713cb3d20f1699436107b72e
Taylor <taylor@baremetalcloud.com>
2021-01-08 00:28:40 -0500

    documentation on bfs vs dfs

---
+++ interviewPrep/BFS&DFS/bfsVsDfs.js
@@ -0,0 +1,21 @@
+/**
+ * BFS:
+ * pros: shortest path & closer nodes
+ * cons: more memory
+ *
+ * DFS:
+ * pros: less memory & does path exist?
+ * cons: can get slow
+ */
+
+//If you know a solution is not far from the root of the tree: BFS
+
+//If the tree is very deep and solutions are rare: BFS
+
+//If the tree is very wide: DFS
+
+//If solutions are frequent but located deep in the tree: DFS
+
+//Determining whether a path exists between two nodes: DFS
+
+//Finding the shortest path: BFS
