e0f07450a778f3760ecc46f0af90aa19ea564b3f
Taylor <taylor@baremetalcloud.com>
2021-01-13 00:41:05 -0500

    Solved is lucky from code signal.

---
+++ codesignal/js/smoothSailing/isLucky.js
@@ -0,0 +1,17 @@
+function isLucky(n) {
+    //Convert number into array of numbers
+    const arr = Array.from(String(n), Number);
+
+    const firstHalf = sum(arr.slice(0, arr.length / 2));
+    const secondHalf = sum(arr.slice(arr.length / 2));
+
+    return firstHalf === secondHalf;
+}
+
+function sum(arr) {
+    return arr.reduce((prev, current) => {
+        const _current = parseInt(current, 0);
+
+        return prev += _current;
+    }, 0)
+}
