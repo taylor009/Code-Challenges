00b4b6d65393ee1a846d4b8831d6a63bc5877dff
Taylor <taylor@baremetalcloud.com>
2021-01-18 23:23:45 -0500

    solved number line jump from hacker rank.

--- hackerRank/problemSolving/numberLineJumps.js
+++ hackerRank/problemSolving/numberLineJumps.js
@@ -1,11 +1,16 @@
 // Complete the kangaroo function below.
 function kangaroo(x1, v1, x2, v2) {
-    let meet = 'NO';
-    for(let i = 1; i <= 1000; i++){
-        if((v1 * i) + x1 === (v2 * i) + x2){
-            meet = 'YES';
-            break;
-        };
+    let kangaroosAreInTheSameSpot = function(jumpNumber) {
+        // Calculates the position of the kangaroos after
+        // a given number of jumps and tests for equality.
+        return (x1 + v1 * jumpNumber == x2 + v2 * jumpNumber);
     }
-    return meet;
+
+    let result = 'NO';
+    for (let i = 0; i < 10000 && result == 'NO'; i++) {
+        if (kangaroosAreInTheSameSpot(i)) {
+            result = 'YES';
+        }
+    }
+    return result;
 }
